d7d3bd1c0798d2d4905e4b52c4597fc3cf7f39b3
GodLRyuk <93247566+GodLRyuk@users.noreply.github.com>
2026-09-25 10:56:40 +0530

    edit: Dropdown removed from Login screen

--- lib/module/globalFeatures/auth/login_screen.dart
+++ lib/module/globalFeatures/auth/login_screen.dart
@@ -106,7 +106,7 @@ class _LoginScreenState extends State<LoginScreen> {
               const SizedBox(height: 28),
 
               // Role selector (dropdown)
-              _roleDropdown(),
+              // _roleDropdown(),
               const SizedBox(height: 16),
 
               AppWidgets.buildTextField(
